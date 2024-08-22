7bea9cf5c420ebe532d308816b4dc3dd96c08695
Anesu Chiodza <anesuchiodza@gmail.com>
2024-08-22 12:06:18 -0400

    library changes again

--- pages/librarytoys.php
+++ pages/librarytoys.php
@@ -65,6 +65,15 @@
 </head>
 
 <body class="g-sidenav-show  bg-gray-200">
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href="#" target="_blank">
+        <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-1 font-weight-bold text-white">Eva Marie Libraries</span>
+      </a>
+    </div>
+    <hr class="horizontal light mt-0 mb-2">
 <?php include './includes/navheader.php'; ?>
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
     <!-- Navbar -->
