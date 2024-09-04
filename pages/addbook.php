8b7268a244f61ab9f5328e52060719e4db16e1a1
Anesu Chiodza <anesuchiodza@gmail.com>
2024-09-04 09:08:50 +0200

    Changes

--- pages/addbook.php
+++ pages/addbook.php
@@ -172,7 +172,8 @@
                 const imageResponse = await fetch('http://203.161.49.218:1337/api/upload', {
                     method: 'POST',
                     headers: {
-                        'Authorization': 'Bearer YOUR_IMAGE_UPLOAD_API_TOKEN_HERE',
+                        'Authorization': 'Bearer 21cacb682481947a85cdb07d7d32580647e58194e373e7287ed4f3a0d4a0101a32080ddc785d8b457509fee2461349f13ae7cfda32837b701cbec6293e4ba92d01613460bf157bb23a161edd2d771f1f783df3fe02d8cda7a83c5d25bc63a04b6f377bed081a8aefa45271d872544fd77755c16c4b042964dae96ff58b4550de
+',
                     },
                     body: formData
                 });
@@ -203,7 +204,7 @@
                 const bookResponse = await fetch('http://203.161.49.218:1337/api/books-metas/', {
                     method: 'POST',
                     headers: {
-                        'Authorization': 'Bearer YOUR_BOOK_META_API_TOKEN_HERE',
+                        'Authorization': 'Bearer 8a751582219d16d9a8a64c10e4b419b9763acb0f90d3b1dcf9ab978308ff4c5585ee8b2fb516b57c86646d2620afe2acff22194957bb09fceccb71e8cbec9850c710eb3c4aecb0257e5839e5235c960e11d3444edd60e0b00e7681d912c5b3d55013f9207d52ee111dc81d861f972e7b5cd25628a8c2f9dba50cceec04dfed25',
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({ data: bookData })
