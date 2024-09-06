3ea489c503fe78a0d38b45f53c9789c988321108
Anesu Chiodza <anesuchiodza@gmail.com>
2024-09-06 12:02:08 +0200

    Changes

--- pages/includes/navheader.php
+++ pages/includes/navheader.php
@@ -42,7 +42,7 @@
         <li class="nav-item">
           <a class="nav-link text-white " href="../index.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">users</i>
+              <i class="material-icons opacity-10">user</i>
             </div>
             <span class="nav-link-text ms-1">Platform Users</span>
           </a>
