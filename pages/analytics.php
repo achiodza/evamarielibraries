0f6cd36675e902c7eb40beeb449bef8cd4d0768b
Anesu Chiodza <anesuchiodza@gmail.com>
2024-09-06 14:05:39 +0200

    Changes

--- pages/analytics.php
+++ pages/analytics.php
@@ -60,7 +60,7 @@
                 <img class="w-10 me-3 mb-0" src="../assets/img/logos.png" alt="logo">
                 <h6 class="mb-0">List of Users In the Our Database</h6>
                 <div class="col-6 text-end">
-                  <a class="btn bg-gradient-dark mb-0" href="javascript:;"><i class="material-icons text-sm">cloud</i>&nbsp;&nbsp;Download</a>
+                  <a class="btn bg-gradient-dark mb-0" href="javascript:;" id="download-excel"><i class="material-icons text-sm">cloud</i>&nbsp;&nbsp;Download</a>
                   </div>
                 </div>
                 </div>
@@ -68,9 +68,8 @@
                 <div class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
                 <img class="w-10 me-3 mb-0" src="../assets/img/logos.png" alt="logo">
                 <h6 class="mb-0">List of Borrowed Books Against Users</h6>
-                <p style="color:red">Coming Soon</p>
                 <div class="col-6 text-end">
-                  <a class="btn bg-gradient-dark mb-0"><i class="material-icons text-sm">cloud</i>&nbsp;&nbsp;Download</a>
+                  <a class="btn bg-gradient-dark mb-0" id="download-borrowedbooks-excel"><i class="material-icons text-sm">cloud</i>&nbsp;&nbsp;Download</a>
                   </div>
                 </div>
                 </div>
@@ -81,6 +80,73 @@
             </div>
         </div>
     </div>
+    <script>
+        const apiUrl = 'http://203.161.49.218:1337/api/borrowedbooks?populate=*';  // Strapi API endpoint
+        const authToken = 'Bearer 8a751582219d16d9a8a64c10e4b419b9763acb0f90d3b1dcf9ab978308ff4c5585ee8b2fb516b57c86646d2620afe2acff22194957bb09fceccb71e8cbec9850c710eb3c4aecb0257e5839e5235c960e11d3444edd60e0b00e7681d912c5b3d55013f9207d52ee111dc81d861f972e7b5cd25628a8c2f9dba50cceec04dfed25';
+
+        // Function to fetch borrowed books data from Strapi API
+        async function fetchBorrowedBooks() {
+            try {
+                const response = await fetch(apiUrl, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': authToken
+                    }
+                });
+                const data = await response.json();
+                return data.data;  // Return the borrowed books data
+            } catch (error) {
+                console.error('Error fetching borrowed books:', error);
+            }
+        }
+
+        // Function to convert JSON data to Excel and trigger download
+        function downloadExcel(borrowedBooks) {
+            const formattedBooks = [];
+
+            // Iterate through each borrowed book record
+            borrowedBooks.forEach(record => {
+                const { userid, createdAt } = record.attributes;
+                const bookDetails = record.attributes.bookdetail;
+
+                // Iterate through each book detail in the borrowed record
+                bookDetails.forEach(book => {
+                    formattedBooks.push({
+                        UserID: userid,
+                        BorrowedAt: createdAt,
+                        BookTitle: book.title,
+                        Author: book.author,
+                        Genre: book.genre,
+                        ISBN: book.isbn,
+                        Publisher: book.publisher,
+                        PublicationDate: book.publicationDate,
+                        Rating: book.rating,
+                        Language: book.language,
+                        TimesBorrowed: book.timesBorrowed || 'N/A',
+                        CoverImageURL: book.coverImage || 'N/A'
+                    });
+                });
+            });
+
+            // Convert JSON data to Excel worksheet
+            const worksheet = XLSX.utils.json_to_sheet(formattedBooks);
+            const workbook = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(workbook, worksheet, 'Borrowed Books');
+
+            // Exporting to Excel file
+            XLSX.writeFile(workbook, 'Borrowed_Books_Data.xlsx');
+        }
+
+        // Add event listener to download button
+        document.getElementById('download-borrowedbooks-excel').addEventListener('click', async () => {
+            const borrowedBooks = await fetchBorrowedBooks();  // Fetch borrowed books data from API
+            if (borrowedBooks && borrowedBooks.length > 0) {
+                downloadExcel(borrowedBooks);  // Trigger download of Excel file
+            } else {
+                alert('No data to download.');
+            }
+        });
+    </script>
     <footer class="footer py-4">
         <div class="container-fluid">
             <div class="row align-items-center justify-content-lg-between">
