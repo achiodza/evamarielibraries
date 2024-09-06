452bea82ffb537161d8ffb622e54fdd400b0efcc
Anesu Chiodza <anesuchiodza@gmail.com>
2024-09-06 12:01:32 +0200

    Changes

--- pages/includes/navheader.php
+++ pages/includes/navheader.php
@@ -49,7 +49,7 @@
         </li>
         
         <li class="nav-item">
-          <a class="nav-link text-white " href="../index.php">
+          <a class="nav-link text-white " href="../pages/users.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">assignment</i>
             </div>
