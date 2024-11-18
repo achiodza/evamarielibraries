050be76e22fa56b6a3fe0d8fdf39baa4527eec56
Anesu Chiodza <anesuchiodza@gmail.com>
2024-11-18 07:17:18 +0200

    Updates

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -229,7 +229,7 @@
             
           </div>
         </div>
-        <div class="col-md-12 mt-4">
+        <div class="col-md-12 mt-8">
           <div class="card">
             <div class="card-header pb-0 px-3">
               <h6 class="mb-0">Borrowing Request Details Queue</h6>
