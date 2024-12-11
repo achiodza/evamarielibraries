a1030a656bfeaba370db5dff3f57ed0b79d25b3f
Anesu Chiodza <anesuchiodza@gmail.com>
2024-12-11 16:03:58 +0200

    Updates

--- pages/librarybooks.php
+++ pages/librarybooks.php
@@ -127,10 +127,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const pageSize = 10; // Number of items per page
 
     // Fetch and populate data with pagination
-    const fetchData = (page) => {
-        const url = `https://admin.evamarielibraries.org/api/books-metas/?populate=*&pagination[page]=${page}&pagination[pageSize]=${pageSize}`;
-        fetchBooks(url);
-    };
+    // const fetchData = (page) => {
+    //     const url = `https://admin.evamarielibraries.org/api/books-metas/?populate=*&pagination[page]=${page}&pagination[pageSize]=${pageSize}`;
+    //     fetchBooks(url);
+    // };
 
  
 // Fetch books from a given URL and populate the table
